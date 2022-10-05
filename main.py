10c7c21ae25d4d64d5ad6d2167f4103cb3fb3525
CJRed1 <88350408+CJRed1@users.noreply.github.com>
2022-10-05 15:56:56 -0400

    Ballin

--- main.py
+++ main.py
@@ -6,8 +6,22 @@ pygame.init()
 
 # Colors
 color_background = (100, 100, 100)
-color_players = (255, 255, 255)
+color_player1 = (255, 0, 0)
+color_player2 = (0, 0, 255)
 color_ball = (255, 255, 0)
+
+# Sizes
+player_width = 15
+player_height = 60
+ball_radius = 10
+
+# Coords
+player1_x = 50
+player1_y = 300
+player2_x = 725
+player2_y = 300
+ball_x = 400
+ball_y = 325
 
 # Window Size
 screen_width = 800
@@ -35,6 +49,15 @@ while running:
     
     # Fill the screen with color
     screen.fill(color_background)
+
+    # Drawing area
+
+    # Define the Players
+    player_1 = pygame.draw.rect(screen, color_player1, (player1_x, player1_y, player_width, player_height))
+    player_2 = pygame.draw.rect(screen, color_player2, (player2_x, player2_y, player_width, player_height))
+    
+    # Draw the Ball
+    ball = pygame.draw.circle(screen, color_ball, (ball_x, ball_y), ball_radius)
 
     # Refresh the window
     pygame.display.flip()
