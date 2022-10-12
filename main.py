3e87de2458e04e13c43c28d0f2d567f3470e0049
CJRed1 <88350408+CJRed1@users.noreply.github.com>
2022-10-12 18:25:54 -0400

    Shit

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ player_width = 15
 player_height = 60
 ball_radius = 10
 
+# Speeds
+player1_vspeed = 0
+player2_vspeed = 0
+ball_hspeed = 0.25
+ball_vspeed = 0.25
+
 # Coords
 player1_x = 50
 player1_y = 300
@@ -47,7 +53,60 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        # Players Key Check
+        if event.type == pygame.KEYDOWN:
+
+            # Player 1
+            if event.key == pygame.K_w:
+                player1_vspeed = -0.5
+
+            if event.key == pygame.K_s:
+                player1_vspeed = 0.5
+
+            # Player 2
+            if event.key == pygame.K_UP:
+                player2_vspeed = -0.5
+
+            if event.key == pygame.K_DOWN:
+                player2_vspeed = 0.5
+
+        if event.type == pygame.KEYUP:
+            
+            # Player 1
+            if event.key == pygame.K_w:
+                player1_vspeed = 0
+
+            if event.key == pygame.K_s:
+                player1_vspeed = 0
+
+            # Player 2
+            if event.key == pygame.K_UP:
+                player2_vspeed = 0
+
+            if event.key == pygame.K_DOWN:
+                player2_vspeed = 0
+
+    if player1_y + player1_vspeed >= 0 and player1_y + player1_vspeed + player_height <= screen_height:
+        player1_y += player1_vspeed
+    if player2_y + player2_vspeed >= 0 and player2_y + player2_vspeed + player_height <= screen_height:
+        player2_y += player2_vspeed
+
+    ball_x += ball_hspeed
+    ball_y += ball_vspeed
+    if ball_x <= 0 or ball_x + ball_radius >= screen_width: 
+        ball_hspeed *= -1
+        ball_x = 400
+        ball_y = 325
     
+    if ball_x <= player1_x + player_width and ball_y >= player1_y and ball_y <= player1_y + player_height:
+        ball_hspeed *= -1
+
+    if ball_x + ball_radius >= player2_x and ball_y >= player2_y and ball_y <= player2_y + player_height:
+        ball_hspeed *= -1
+
+    if ball_y <= 0 or ball_y + ball_radius >= screen_height: ball_vspeed *= -1
+
     # Fill the screen with color
     screen.fill(color_background)
 
